3e68bbfaaad08ba08c26f1e577296f6b40ca7415
Faardheen Khodabuccus <faardheenk@frci.net>
2025-04-17 14:39:49 +0400

    initial commit

---
+++ src/webparts/reconciliation/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IReconciliationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ReconciliationWebPartStrings' {
+  const strings: IReconciliationWebPartStrings;
+  export = strings;
+}
